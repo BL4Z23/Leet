fba08a90390194d4e524faf3692a31dfc667e62c
Rehan Gupta <rehangupta253@gmail.com>
2026-09-01 15:58:05 +0530

    Time: 2 ms (10.09%), Space: 19.6 MB (5.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,11 @@
+from math import comb
 class Solution:
     def generate(self, numRows: int) -> list[list[int]]:
         ans = []
         if numRows == 0:
             return []
-        ans.append([1])
-        for i in range(1, numRows):
-            if i != 1:
-                ans.append([1])
-                for j in range(i-1):
-                    ans[i].append(int(ans[i-1][j]) + int(ans[i-1][j+1]))
-                ans[i].append(1)
-            else:
-                ans.append([1, 1])
+        for i in range(0, numRows):
+            ans.append([])
+            for j in range(i+1):
+                ans[i].append(comb(i, j))
         return ans
-        
