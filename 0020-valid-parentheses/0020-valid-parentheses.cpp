15c0c46e031a34cc2cc840e1d9ec6cc7b2410b44
Rehan Gupta <rehangupta253@gmail.com>
2026-09-03 18:31:11 +0530

    Time: 2 ms (15.07%), Space: 9 MB (18.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> d;
+        stack<char> sta;
+        d['('] = ')';
+        d['{'] = '}';
+        d['['] = ']';
+        for (char c : s) {
+            if (d.contains(c)) {
+                sta.push(d[c]);
+            } else {
+                if (!sta.empty()) {
+                    if (sta.top() == c) {
+                        sta.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (!sta.empty()) {
+            return false;
+        } else {
+            return true;
+        };
+    }
+};
