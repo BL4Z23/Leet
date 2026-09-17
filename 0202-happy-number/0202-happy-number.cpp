43d0d5a39d56845aecdc6f43a76b78ee096c5825
Rehan Gupta <rehangupta253@gmail.com>
2026-09-17 21:49:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (21.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isHappy(int n) {
         int digit =  0;
         int split;
-        set<int> seen;
+        unordered_set<int> seen;
         while (n != 1) {
             split = 0;
             while (n>0) {
