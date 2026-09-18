a6e2ad9cf62ff41ff49f82350f93de8ac233862f
Rehan Gupta <rehangupta253@gmail.com>
2026-09-18 08:55:20 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
+        int num = 1;
         int comma = 0;
         for (int num = 1; num <= 2; ++num ) {
             if (n >= pow(1000, num)) {
