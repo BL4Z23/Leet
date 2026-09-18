c2000f5ad00f5f21b8957174cd6a1b4a129f5c7f
Rehan Gupta <rehangupta253@gmail.com>
2026-09-18 09:41:23 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.34%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
-        std::unordered_set<std::string> op = {"+", "-", "*", "/"};
         int a, b;
-        for (int i=0; i < tokens.size(); ++i) {
-            if (op.count(tokens[i])) {
+        for (int i = 0; i < tokens.size(); ++i) {
+            if (tokens[i] == "+" || tokens[i] == "-" ||
+    tokens[i] == "*" || tokens[i] == "/") {
                 b = s.top();
                 s.pop();
                 a = s.top();
                 s.pop();
                 switch (tokens[i][0]) {
-                    case '+':
-                        s.push(a + b);
-                        break;
+                case '+':
+                    s.push(a + b);
+                    break;
 
-                    case '-':
-                        s.push(a - b);
-                        break;
+                case '-':
+                    s.push(a - b);
+                    break;
 
-                    case '*':
-                        s.push(a * b);
-                        break;
+                case '*':
+                    s.push(a * b);
+                    break;
 
-                    case '/':
-                        s.push(a / b);
-                        break;
+                case '/':
+                    s.push(a / b);
+                    break;
                 }
 
             } else {
