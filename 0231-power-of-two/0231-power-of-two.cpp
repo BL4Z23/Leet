7eddbd64799386ed06db4981369b1fd1bc5a40e6
Rehan Gupta <rehangupta253@gmail.com>
2026-09-19 13:25:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n > 0) {
-            return log2(n) == floor(log2(n));
-        } else {
-            return false;
-        }
-        
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
