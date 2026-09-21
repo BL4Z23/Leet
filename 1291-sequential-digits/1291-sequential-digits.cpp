c9f10e72f7240fee1a3a822f9f7bfc7ef99105bf
Rehan Gupta <rehangupta253@gmail.com>
2026-09-21 21:54:06 +0530

    Time: 0 ms (100%), Space: 8.3 MB (70.86%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         std::string dic = "123456789";
         vector<int> ans;
-        int lowLen = std::to_string(low).size();
-        int highLen = std::to_string(high).size();
-        for (int i = lowLen; i <= highLen && i <= 9; ++i) {
+        for (int i = std::to_string(low).size(); i <= std::to_string(high).size() && i <= 9; ++i) {
             for (int j = 0; j + i <= 9; ++j) {
                 int num = stoi(dic.substr(j, i));
                 if (low <= num && num <= high) {
