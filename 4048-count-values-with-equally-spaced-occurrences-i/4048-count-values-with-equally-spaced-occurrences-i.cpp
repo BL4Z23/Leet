c44340dd20a22886265ada5672408901c20725d0
Rehan Gupta <rehangupta253@gmail.com>
2026-09-26 16:51:56 +0530

    Time: 0 ms (100%), Space: 32 MB (74.08%) - LeetHub

--- 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -7,10 +7,8 @@ public:
             ind[nums[i]].push_back(i);
         }
         for (auto& [key, values] : ind) {
-            if (values.size() == 3) {
-                if (values[1] - values[0] == values[2] - values[1]) {
-                    ++ans;
-                }
+            if (values.size() == 3 && values[1] - values[0] == values[2] - values[1]) {
+                ++ans;
             }
         }
         return ans;
